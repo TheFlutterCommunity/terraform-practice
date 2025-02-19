c027b31eadad138c8241ec280f1bb49c1c58c303
DnXLogic <vijay11392@gmail.com>
2025-02-19 10:31:46 +0530

    Added lambda function module

--- terraform-7-lambda/main.tf
+++ terraform-7-lambda/main.tf
@@ -1,3 +1,42 @@
+# IAM Role for Lambda
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_execution_role"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
 
+# IAM Policy for Lambda Execution
+resource "aws_iam_policy_attachment" "lambda_logs" {
+  name       = "lambda_logs_attachment"
+  roles      = [aws_iam_role.lambda_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
 
+# Upload Lambda Function
+resource "aws_lambda_function" "my_lambda" {
+  function_name    = "my_lambda_function"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.12"
+  timeout          = 900
+  memory_size      = 128
+  filename         = "lambda_function.zip"
+  source_code_hash = filebase64sha256("lambda_function.zip")
+}
+
+# Allow invoking the Lambda function
+resource "aws_lambda_permission" "allow_api_gateway" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.my_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+}
